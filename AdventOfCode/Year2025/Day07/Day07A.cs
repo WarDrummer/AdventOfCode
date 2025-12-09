1a4d2d49af0d1df045f08efced4d650c3672060c
Jason Pike <jason.pike@stellasource.com>
2025-12-09 10:05:08 -0600

    day 7 2025

--- AdventOfCode/Year2025/Day07/Day07A.cs
+++ AdventOfCode/Year2025/Day07/Day07A.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using AdventOfCode.Problem;
 
 namespace AdventOfCode.Year2025.Day07;
@@ -6,7 +8,47 @@ public class Day07A : ProblemWithInput<Day07A>
 {
     public override string Solve()
     {
-        // var data = ParserFactory.CreateMultiLineStringParser().GetData();
-        return "Not Solved";
+        var data = ParserFactory.CreateMultiLineStringParser().GetData().ToList();
+
+        Point start = new(0, 0);
+        var splitters = new HashSet<Point>();
+        
+        for (var y = 0; y < data.Count; y++)
+        {
+            var line = data[y];
+            for (var x = 0; x < line.Length; x++)
+            {
+                var c = line[x];
+                if(c == 'S')
+                    start = new(x, y);
+                else if(c == '^')
+                    splitters.Add(new(x, y));
+            }
+        }
+
+        var numOfSplits = 0;
+        var maxY = data.Count;
+        var beams = new HashSet<Point> { start };
+        for (var i = start.Y; i <= maxY; i++)
+        {
+            var nextBeams = new HashSet<Point>();
+            foreach (var beam in beams)
+            {
+                var nextBeam = beam.South();
+                if (splitters.Contains(nextBeam))
+                {
+                    numOfSplits++;
+                    nextBeams.Add(nextBeam.East());
+                    nextBeams.Add(nextBeam.West());
+                }
+                else
+                {
+                    nextBeams.Add(nextBeam);
+                }
+            }
+            beams = nextBeams;
+        }
+        
+        return numOfSplits.ToString();
     }
 }
--- AdventOfCode/Year2025/Day07/Day07B.cs
+++ AdventOfCode/Year2025/Day07/Day07B.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using AdventOfCode.Problem;
 
 namespace AdventOfCode.Year2025.Day07
@@ -6,8 +8,51 @@ namespace AdventOfCode.Year2025.Day07
     {
         public override string Solve()
         {
-            // var data = ParserFactory.CreateMultiLineStringParser().GetData();
-            return "Not Solved";
+            var data = ParserFactory.CreateMultiLineStringParser().GetData().ToList();
+
+            Point start   = new(0, 0);
+            var splitters = new HashSet<Point>();
+
+            for (var y = 0; y < data.Count; y++)
+            {
+                var line = data[y];
+                for (var x = 0; x < line.Length; x++)
+                {
+                    var c = line[x];
+                    if (c == 'S')
+                        start = new(x, y);
+                    else if (c == '^')
+                        splitters.Add(new(x, y));
+                }
+            }
+
+            _cache.Clear();
+            var numOfSplits = Split(start, splitters, data.Count);
+
+            return numOfSplits.ToString();
+        }
+
+        private readonly Dictionary<(Point, int), ulong> _cache = new();
+
+        private ulong Split(Point beam, HashSet<Point> splitters, int maxY)
+        {
+            if (beam.Y >= maxY) 
+                return 1;
+
+            var key = (beam, maxY);
+            if (_cache.ContainsKey(key)) 
+                return _cache[key];
+
+            var next = beam.South();
+            ulong result;
+            if (splitters.Contains(next))
+                result = Split(next.East(), splitters, maxY) + Split(next.West(), splitters, maxY);
+            else
+                result = Split(next, splitters, maxY);
+
+            _cache[key] = result;
+            return result;
         }
     }
+
 }
