5f89b60bc64ce5134f8a426d51ba8149d2da51b0
Jason Pike <jason.pike@stellasource.com>
2025-12-09 11:53:15 -0600

    cleanup day 8

--- AdventOfCode/Year2025/Day08/Day08A.cs
+++ AdventOfCode/Year2025/Day08/Day08A.cs
@@ -9,9 +9,9 @@ namespace AdventOfCode.Year2025.Day08
     {
         private struct JunctionBox
         {
-            public long X { get; init; }
-            public long Y { get; init; }
-            public long Z { get; init; }
+            public int X { get; init; }
+            public int Y { get; init; }
+            public int Z { get; init; }
 
             public double DistanceTo(JunctionBox box)
             {
@@ -19,66 +19,88 @@ namespace AdventOfCode.Year2025.Day08
             }
         }
         
+        private struct Connection
+        {
+            public double Distance { get; init; }
+            public JunctionBox Box1 { get; init; }
+            public JunctionBox Box2 { get; init; }
+        }
+        
         public override string Solve()
         {
             var jBoxes = ParserFactory.CreateMultiLineStringParser().GetData()
                 .Select(coords => coords.Split(',')
-                    .Select(long.Parse).ToArray())
+                    .Select(int.Parse).ToArray())
                 .Select(arr => new JunctionBox {X = arr[0], Y = arr[1], Z = arr[2]}).ToArray();
 
-            var connections = new List<Tuple<double, JunctionBox, JunctionBox>>();
+            var connections = new List<Connection>();
             for (var i = 0; i < jBoxes.Length; i++)
             {
-                for (var j = i + 1; j < jBoxes.Length; j++)
+                for (var j = 0; j < jBoxes.Length; j++)
                 {
+                    if(i == j) continue; // Don't count connections between the same box (e.g. box 0 to box 0'
                     var distance = jBoxes[i].DistanceTo(jBoxes[j]);
-                    connections.Add(new Tuple<double, JunctionBox, JunctionBox>(
-                        distance, jBoxes[i], jBoxes[j]));
+                    connections.Add(new Connection {
+                        Distance = distance, 
+                        Box1 = jBoxes[i], 
+                        Box2 = jBoxes[j]
+                    });
                 }
             }
             
-            connections.Sort((a, b) => a.Item1.CompareTo(b.Item1));
+            connections.Sort((a, b) => a.Distance.CompareTo(b.Distance));
             
             var circuits = new List<HashSet<JunctionBox>>();
             var connectionCount = 0;
             var numConnectionsToMake = 1000; // CHANGE TO 1000 (or 10 for sample)
             foreach(var connection in connections)
             {
-                var box = connection.Item2;
-                var closest = connection.Item3;
-                var found = false;
-                foreach (var circuit in circuits)
-                {
-                    if (circuit.Contains(box) && circuit.Contains(closest))
-                    {
-                        found = true;
-                        break;
-                    }
+                var box1 = connection.Box1;
+                var box2 = connection.Box2;
+                
+                // Find the circuits containing each box
+                var circuit1 = circuits.FirstOrDefault(c => c.Contains(box1));
+                var circuit2 = circuits.FirstOrDefault(c => c.Contains(box2));
 
-                    if (circuit.Contains(box) || circuit.Contains(closest))
-                    {
-                        circuit.Add(box);
-                        circuit.Add(closest);
-                        found = true;
-                        connectionCount++;
-                        break;
-                    }
+                // Neither box is in a circuit, create a new one circuit
+                if (circuit1 == null && circuit2 == null)
+                {
+                    circuits.Add(new HashSet<JunctionBox> { box1, box2 });
+                    connectionCount++;
                 }
-
-                if (!found)
+                // Only box1 is in a circuit, add box2 to it
+                else if (circuit1 != null && circuit2 == null)
                 {
-                    circuits.Add(new HashSet<JunctionBox> {box, closest});
+                    circuit1.Add(box2);
+                    connectionCount++;
+                }
+                // Only box2 is in a circuit, add box1 to it
+                else if (circuit1 == null && circuit2 != null)
+                {
+                    circuit2.Add(box1);
+                    connectionCount++;
+                }
+                // Both boxes in the same circuit (no need to merge)
+                else if (circuit1 == circuit2)
+                {
+                    continue;
+                }
+                // Both boxes are in different circuits, merge them
+                else
+                {
+                    circuit1.UnionWith(circuit2);  // Merge circuit2 into circuit1
+                    circuits.Remove(circuit2);     // Remove the now-empty circuit2
                     connectionCount++;
                 }
 
-                if (connectionCount == numConnectionsToMake)
+                if (connectionCount == numConnectionsToMake - 1) 
                     break;
             }
             
             // 5504268 too high
             // 8160 too low
-            var sortedCircuits = circuits.OrderBy(c => c.Count).ToList();
-            var total = sortedCircuits[^1].Count * sortedCircuits[^2].Count * sortedCircuits[^3].Count;
+            var sortedCircuits = circuits.OrderByDescending(c => c.Count).ToList();
+            var total = sortedCircuits[0].Count * sortedCircuits[1].Count * sortedCircuits[2].Count;
             return total.ToString();
         }
     }
