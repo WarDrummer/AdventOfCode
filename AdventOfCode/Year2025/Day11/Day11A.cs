f19a7017d35c981f675562c64e9b05fa53e220c1
Jason Pike <jason.pike@stellasource.com>
2025-12-11 10:36:08 -0600

    day 11a, too slow

--- AdventOfCode/Year2025/Day11/Day11A.cs
+++ AdventOfCode/Year2025/Day11/Day11A.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using AdventOfCode.Problem;
 
 namespace AdventOfCode.Year2025.Day11
@@ -6,8 +9,71 @@ namespace AdventOfCode.Year2025.Day11
     {
         public override string Solve()
         {
-            // var data = ParserFactory.CreateMultiLineStringParser().GetData();
-            return "Not Solved";
+            var data = ParserFactory.CreateMultiLineStringParser().GetData()
+                .Select(s => s.Split(":", StringSplitOptions.TrimEntries));
+            
+            var outToYouMappings = new Dictionary<string, HashSet<string>>();
+            foreach (var mapping in data)
+            {
+                var to = mapping[0];
+                var froms = mapping[1].Split(" ", StringSplitOptions.TrimEntries);
+
+                foreach (var from in froms)
+                {
+                    if (!outToYouMappings.ContainsKey(from))
+                        outToYouMappings.Add(from, new HashSet<string>());
+                    outToYouMappings[from].Add(to);
+                }
+            }
+
+            var count = 0;
+            foreach (var mapping in outToYouMappings["out"])
+            {
+               count += FindPaths(mapping, "you", outToYouMappings).Sum();
+               // count += FindPathsVerbose(mapping, "you", outToYouMappings, "out->" + mapping).Sum();
+            }
+
+            return count.ToString();
+        }
+
+        private static IEnumerable<int> FindPaths(string from, string to, Dictionary<string, HashSet<string>> mappings)
+        {
+            if (from == to)
+            {
+                yield return 1;
+            }
+
+            if (mappings.ContainsKey(from))
+            {
+                foreach (var f in mappings[from])
+                {
+                    foreach(var p in FindPaths(f, to, mappings))
+                    {
+                        yield return 1;
+                    }
+                }
+            }
+        }
+        
+        private static IEnumerable<int> FindPathsVerbose(string from, string to, Dictionary<string, HashSet<string>> mappings,
+            string path)
+        {
+            if (from == to)
+            {
+                Console.WriteLine(path);
+                yield return 1;
+            }
+
+            if (mappings.ContainsKey(from))
+            {
+                foreach (var f in mappings[from])
+                {
+                    foreach(var p in FindPathsVerbose(f, to, mappings, $"{path}->{f}"))
+                    {
+                        yield return 1;
+                    }
+                }
+            }
         }
     }
 }
