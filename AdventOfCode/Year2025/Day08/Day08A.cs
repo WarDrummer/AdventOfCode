37dab2e3d2a5c6271ff18bbeed1fa95650f9ad0f
Jason Pike <jason.pike@stellasource.com>
2025-12-11 21:05:50 -0600

    day8a and b 2025

--- AdventOfCode/Year2025/Day08/Day08A.cs
+++ AdventOfCode/Year2025/Day08/Day08A.cs
@@ -7,7 +7,7 @@ namespace AdventOfCode.Year2025.Day08
 {
     public class Day08A : ProblemWithInput<Day08A>
     {
-        private struct JunctionBox
+        protected struct JunctionBox
         {
             public int X { get; init; }
             public int Y { get; init; }
@@ -15,11 +15,11 @@ namespace AdventOfCode.Year2025.Day08
 
             public double DistanceTo(JunctionBox box)
             {
-                return Math.Sqrt((box.X - X) * (box.X - X) + (box.Y - Y) * (box.Y - Y) + (box.Z - Z) * (box.Z - Z));
+                return Math.Pow(box.X - X, 2) + Math.Pow(box.Y - Y, 2) + Math.Pow(box.Z - Z, 2);
             }
         }
         
-        private struct Connection
+        protected struct Connection
         {
             public double Distance { get; init; }
             public JunctionBox Box1 { get; init; }
@@ -28,11 +28,31 @@ namespace AdventOfCode.Year2025.Day08
         
         public override string Solve()
         {
-            var jBoxes = ParserFactory.CreateMultiLineStringParser().GetData()
-                .Select(coords => coords.Split(',')
-                    .Select(int.Parse).ToArray())
-                .Select(arr => new JunctionBox {X = arr[0], Y = arr[1], Z = arr[2]}).ToArray();
+            var jBoxes = GetJunctionBoxes();
+            var connections = GetConnections(jBoxes);
+            var circuits = jBoxes.Select(jbox => new HashSet<JunctionBox> { jbox }).ToList();
+            
+            var numConnectionsToMake = 1000; // CHANGE TO 1000 (or 10 for sample)
+            for (var i = 0; i < numConnectionsToMake; i++)
+            {
+                var connection = connections[i];
+                var circuit1 = circuits.FirstOrDefault(c => c.Contains(connection.Box1));
+                var circuit2 = circuits.FirstOrDefault(c => c.Contains(connection.Box2));
 
+                if (circuit1 != circuit2)
+                {
+                    circuit1.UnionWith(circuit2); // Merge circuit2 into circuit1
+                    circuits.Remove(circuit2); // Remove the now-empty circuit2
+                }
+            }
+
+            var sortedCircuits = circuits.OrderByDescending(c => c.Count).ToList();
+            var total = sortedCircuits[0].Count * sortedCircuits[1].Count * sortedCircuits[2].Count;
+            return total.ToString();
+        }
+
+        protected static List<Connection> GetConnections(JunctionBox[] jBoxes)
+        {
             var connections = new List<Connection>();
             for (var i = 0; i < jBoxes.Length; i++)
             {
@@ -48,57 +68,16 @@ namespace AdventOfCode.Year2025.Day08
             }
             
             connections.Sort((a, b) => a.Distance.CompareTo(b.Distance));
-            
-            var circuits = new List<HashSet<JunctionBox>>();
-            var connectionCount = 0;
-            var numConnectionsToMake = 10; // CHANGE TO 1000 (or 10 for sample)
-            foreach(var connection in connections)
-            {
-                var box1 = connection.Box1;
-                var box2 = connection.Box2;
-                
-                // Find the circuits containing each box
-                var circuit1 = circuits.FirstOrDefault(c => c.Contains(box1));
-                var circuit2 = circuits.FirstOrDefault(c => c.Contains(box2));
-                
-                connectionCount++;
-                
-                // Neither box is in a circuit, create a new circuit
-                if (circuit1 == null && circuit2 == null)
-                {
-                    circuits.Add(new HashSet<JunctionBox> { box1, box2 });
-                }
-                // Only box1 is in a circuit, add box2 to it
-                else if (circuit1 != null && circuit2 == null)
-                {
-                    circuit1.Add(box2);
-                }
-                // Only box2 is in a circuit, add box1 to it
-                else if (circuit1 == null && circuit2 != null)
-                {
-                    circuit2.Add(box1);
-                }
-                // Both boxes in the same circuit (no need to merge)
-                else if (circuit1 == circuit2)
-                {
-                    continue;
-                }
-                // Both boxes are in different circuits, merge them
-                else
-                {
-                    circuit1.UnionWith(circuit2);  // Merge circuit2 into circuit1
-                    circuits.Remove(circuit2);     // Remove the now-empty circuit2
-                }
+            return connections;
+        }
 
-                if (connectionCount == numConnectionsToMake) 
-                    break;
-            }
-            
-            // 5504268 too high
-            // 8160 too low
-            var sortedCircuits = circuits.OrderByDescending(c => c.Count).ToList();
-            var total = sortedCircuits[0].Count * sortedCircuits[1].Count * sortedCircuits[2].Count;
-            return total.ToString();
+        protected JunctionBox[] GetJunctionBoxes()
+        {
+            var jBoxes = ParserFactory.CreateMultiLineStringParser().GetData()
+                .Select(coords => coords.Split(',')
+                    .Select(int.Parse).ToArray())
+                .Select(arr => new JunctionBox {X = arr[0], Y = arr[1], Z = arr[2]}).ToArray();
+            return jBoxes;
         }
     }
 }
--- AdventOfCode/Year2025/Day08/Day08B.cs
+++ AdventOfCode/Year2025/Day08/Day08B.cs
@@ -1,13 +1,36 @@
+using System.Collections.Generic;
+using System.Linq;
 using AdventOfCode.Problem;
 
 namespace AdventOfCode.Year2025.Day08
 {
-    public class Day08B : ProblemWithInput<Day08B>
+    public class Day08B : Day08A
     {
         public override string Solve()
         {
-            // var data = ParserFactory.CreateMultiLineStringParser().GetData();
-            return "Not Solved";
+            var jBoxes = GetJunctionBoxes();
+            var connections = GetConnections(jBoxes);
+            var circuits = jBoxes.Select(jbox => new HashSet<JunctionBox> { jbox }).ToList();
+
+            JunctionBox box1, box2;
+            var i = 0;
+            do
+            {
+                var connection = connections[i++];
+                box1 = connection.Box1;
+                box2 = connection.Box2;
+                var circuit1 = circuits.FirstOrDefault(c => c.Contains(box1));
+                var circuit2 = circuits.FirstOrDefault(c => c.Contains(box2));
+
+                if (circuit1 != circuit2)
+                {
+                    circuit1.UnionWith(circuit2); // Merge circuit2 into circuit1
+                    circuits.Remove(circuit2); // Remove the now-empty circuit2
+                }
+            } while (circuits.Count > 1);
+
+            var total = box1.X * box2.X;
+            return total.ToString();
         }
     }
 }
